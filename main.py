226d3cee52f83fe8f846feafdc4399e3f4657fd5
maciekadamczykk <maciek.adamczyk2004@gmail.com>
2025-06-23 18:03:46 +0200

    0.97 acc

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ df = pd.read_csv("breast_cancer.csv")
 
 X = df.iloc[:,1:-1].to_numpy()
 y = df.iloc[:,-1].to_numpy()
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(random_state=42)
+lr.fit(X_train,y_train)
+
+y_pred = lr.predict(X_test)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+c_matrix = confusion_matrix(y_pred,y_test)
+sns.heatmap(c_matrix,annot=True)
+plt.show()
+acc = accuracy_score(y_pred,y_test)
+print(acc)
