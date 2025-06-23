260ab9bc7a99694a2371df3b84923920093e31a2
maciekadamczykk <maciek.adamczyk2004@gmail.com>
2025-06-23 18:18:04 +0200

    feature scaling and plotting the results ~98% acc

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("breast_cancer.csv") 
 
+print(df["Class"].nunique())
+
 X = df.iloc[:,1:-1].to_numpy()
 y = df.iloc[:,-1].to_numpy()
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=777)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
 from sklearn.linear_model import LogisticRegression
 
 lr = LogisticRegression(random_state=42)
-lr.fit(X_train,y_train)
+lr.fit(X_train_scaled,y_train)
 
-y_pred = lr.predict(X_test)
+y_pred = lr.predict(X_test_scaled)
 
 from sklearn.metrics import accuracy_score, confusion_matrix
 import seaborn as sns
 c_matrix = confusion_matrix(y_pred,y_test)
 sns.heatmap(c_matrix,annot=True)
 plt.show()
-acc = accuracy_score(y_pred,y_test)
+acc = accuracy_score(y_test,y_pred)
 print(acc)
